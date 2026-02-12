fbed578ad2866636a0913d31a5ceda76c40a79d0
La Taunae Brooks <brookslataunae1@gmail.com>
2026-02-11 21:06:33 -0800

    Add cache busting to force fresh deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Force cache busting
+  generateBuildId: async () => {
+    return `build-${Date.now()}`
+  }
+}
 
 module.exports = nextConfig
+
 
 
 
